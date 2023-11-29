c4445d66340e81f9b2b1da6570d4facf57de262b
Yogurt7v <egorov-2k@yandex.ru>
2023-11-29 19:11:55 +0300

    делаю валидацию

--- src/components/form.jsx
+++ src/components/form.jsx
@@ -1,25 +1,61 @@
+import React, { useState } from "react";
+import style from "./form.module.css";
 
-import  style from './form.module.css';
+const sendFormToConsole = (formData) => {
+  console.log(formData);
+};
 export default function Form() {
+  const [email, setEmail] = useState("");
+  const [emailErorr, setEmailError] = useState(null);
+
+  const [password, setPassword] = useState("");
+  const [passwordError, setPasswordError] = useState(null);
+
+  const [checkPassword, setCheckPassword] = useState("");
+
+  const getState = () => {
+    return {
+      email,
+      password,
+      checkPassword,
+    };
+  };
+
+  const onSubmit = (event) => {
+    event.preventDefault();
+    sendFormToConsole(getState());
+  };
+
   return (
     <>
-    <form>
-      <label>
-         <input type="text" placeholder="Email" />
-      </label>
-      <label>
-         <input type="password" placeholder="Password" />
-      </label>
-      <label>
-       <input type="password" placeholder="Check password" />
-      </label>
+      <form>
+        <label>
+          <input
+            type="email"
+            placeholder="Email"
+            value={email}
+            onChange={({ target }) => setEmail(target.value)}
+          />
+        </label>
+        <label>
+          <input
+            type="password"
+            placeholder="Password"
+            value={password}
+            onChange={({ target }) => setPassword(target.value)}
+          />
+        </label>
+        <label>
+          <input
+            type="password"
+            placeholder="Check password"
+            value={checkPassword}
+            onChange={({ target }) => setCheckPassword(target.value)}
+          />
+        </label>
 
-      <button class={style.red} type="button">
-        Зарегистрироваться
-      </button>
-    </form>
-
+        <button onClick={onSubmit}>Зарегистрироваться</button>
+      </form>
     </>
   );
 }
-
